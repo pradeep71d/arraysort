d1d1901a5dacdc50c4d51e944c1278a1f268b9d4
sandeep <sandeep@ashmar.in>
2019-09-24 13:40:09 +0530

    add

---
+++ sortinglib/src/main/java/com/example/sortinglib/ArraysSort.java
@@ -0,0 +1,12 @@
+package com.example.sortinglib;
+
+import java.util.Arrays;
+
+public class ArraysSort {
+ public String show(int[] a){
+
+     Arrays.sort(a);
+    return Arrays.toString(a);
+
+    }
+}
